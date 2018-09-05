451b58e20acdec572c9f40c12d87304a1c9d0caf
sharktamer <donohoe.shane@gmail.com>
2018-09-05 03:08:02 +0100

    sort albums by mtime

--- rofi-mopidy-spotify_fs.py
+++ rofi-mopidy-spotify_fs.py
@@ -6,20 +6,19 @@ from subprocess import Popen, PIPE
 with open('fs.json') as f:
     fsj = json.load(f)
 
+fsj = sorted(fsj, key=lambda x: x['mtime'], reverse=True)
+
 albums = '\n'.join('{} - {}'.format(i['artist'], i['album']) for i in fsj)
 
 rofi_cmd = 'rofi -dmenu -p Music -i -no-custom -format i'.split()
 caller = Popen(args=rofi_cmd, stdin=PIPE, stdout=PIPE)
 stdout, stderr = caller.communicate(input=albums.encode())
-print(stdout, stderr, caller.returncode)
 
 # Add given uri to mpd playlist using mpc (TODO: use mopidy through python)
 def add_uri(uri):
-    print('mpc add {}'.format(uri).split())
     Popen(args='mpc add {}'.format(uri).split())
 
 
 album = fsj[int(stdout.strip())]
 for i in sorted(album['tracks'], key=lambda x: x['uri']):
-    print(i['uri'])
     add_uri(i['uri'])
