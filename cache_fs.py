1efc9d91dbd9296d31e644dc14498406b1cb3891
sharktamer <donohoe.shane@gmail.com>
2018-09-05 03:07:33 +0100

    add mtime to album, add type to both

--- cache_fs.py
+++ cache_fs.py
@@ -2,9 +2,8 @@
 
 import os
 from tinytag import TinyTag
-from itertools import groupby, tee
+from itertools import groupby
 from operator import itemgetter
-import copy
 import json
 import urllib.parse
 
@@ -26,20 +25,16 @@ so = [{'artist': s.artist,
        'track': int(s.track) if (s.track and s.track.isdigit()) else 0,
        'title': s.title,
        'uri': 'file://{}'.format(urllib.parse.quote(f)),
-       'mtime': os.path.getmtime(f)} for s, f in songs]
+       'mtime': os.path.getmtime(f),
+       'type': 'file'} for s, f in songs]
 
-
-# grouper = itemgetter('albumartist', 'album')
-# for key, grp in groupby(sorted(so, key=grouper), grouper):
-#     print(key)
-#     for item in grp:
-#         print(item)
-#     break
 
 grouper = itemgetter('albumartist', 'album')
-sa = [{'artist': key[0],
-       'album': key[1],
-       'tracks': [i for i in grp]} for key, grp in groupby(so, grouper)]
+gb = [(k, list(g)) for k, g in groupby(sorted(so, key=grouper), grouper)]
+sa = [{'artist': k[0],
+       'album': k[1],
+       'mtime': max(i['mtime'] for i in g),
+       'tracks': [i for i in g]} for k, g in gb]
 
 with open('fs.json', 'w') as f:
     json.dump(sa, f)
