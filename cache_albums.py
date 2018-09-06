13ed060bf0f140dd2d1b867eab96cfd15b5cff8b
sharktamer <donohoe.shane@gmail.com>
2018-09-06 14:16:15 +0100

    new parse functions to match fs.json

--- cache_albums.py
+++ cache_albums.py
@@ -7,6 +7,7 @@ import argparse
 from configparser import ConfigParser, NoOptionError
 import spotipy
 import spotipy.util
+import time
 
 # Set defaults for config and output
 cfgfile = os.path.expanduser('~/.config/rofi-mopidy-spotify/api.conf')
@@ -50,7 +51,7 @@ try:
         'client_secret': config.get('api', 'client_secret'),
         'redirect_uri': config.get('api', 'redirect_uri'),
         'scope': 'user-library-read',
-        'cache_path': api_cache
+        #'cache_path': api_cache
     }
 except NoOptionError as err:
     print('Error: Missing values in api.conf', file=sys.stderr)
@@ -67,25 +68,53 @@ if not token:
 if os.path.exists(output):
     os.remove(output)
 
-# Function for writing albums from api to file
-def get_albums(results):
-    offset = results['offset']
-    count = len(results['items'])
-    print('Collecting albums {}-{}'.format(offset, offset + count))
+def results_gen(r):
+    while r:
+        for i in r['items']:
+            yield i
+        r = sp.next(r)
 
-    for item in results['items']:
-        yield {'artist': ', '.join(i['name'] for i in item['album']['artists']),
-               'album': item['album']['name'],
-               'uri': item['album']['uri'],
-               'date': item['added_at']}
+def dt_to_mtime(dt):
+    pattern = '%Y-%m-%dT%H:%M:%SZ'
+
+    return int(time.mktime(time.strptime(dt, pattern)))
+
+def track_to_dict(t, albumartist, album, mtime):
+    artist = ', '.join(i['name'] for i in t['artists'])
+    track = float('{}.{}'.format(t['disc_number'], t['track_number']))
+    title =  t['name']
+    uri = t['uri']
+    mtime = mtime
+
+    return {'artist': artist,
+            'albumartist': albumartist,
+            'album': album,
+            'track': track,
+            'title': title,
+            'uri': uri,
+            'mtime': mtime,
+            'type': 'spotify'}
+
+def album_to_dict(a):
+    aa = a['album']
+    artist = ', '.join(i['name'] for i in aa['artists'])
+    album = aa['name']
+    mtime = dt_to_mtime(a['added_at'])
+    tracks = [track_to_dict(i, artist, album, mtime)
+              for i in results_gen(aa['tracks'])]
+    uri = aa['uri']
+
+    return {'artist': artist,
+            'album': album,
+            'mtime': mtime,
+            'tracks': tracks,
+            'type': 'spotify',
+            'uri': uri}
 
 sp = spotipy.Spotify(auth=token)
 results = sp.current_user_saved_albums(limit=50)
+
+albums = [album_to_dict(i) for i in results_gen(results)]
+
 with open(output, 'w') as f:
-    #  writer = csv.writer(f)
-    albums = [i for i in get_albums(results)]
-    while results['next']:
-        results = sp.next(results)
-        albums += [i for i in get_albums(results)]
-    print('Writing albums to {}'.format(output))
     json.dump(albums, f)
