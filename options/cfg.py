d0d95125181b089d0bb182b37844681cd84dd607
sharktamer <donohoe.shane@gmail.com>
2018-09-07 15:19:30 +0100

    readfp deprecated, use read_file

--- options/cfg.py
+++ options/cfg.py
@@ -5,7 +5,7 @@ cfgfile = '{}/api.conf'.format(args.conf_dir)
 
 config = ConfigParser()
 try:
-    config.readfp(open(cfgfile))
+    config.read_file(open(cfgfile))
 except FileNotFoundError:
     print('Error: {} not found'.format(cfgfile), file=sys.stderr)
     sys.exit(1)
