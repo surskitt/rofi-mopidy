66efcc6e9318d75767484aa280468ea76dfca656
sharktamer <donohoe.shane@gmail.com>
2018-09-07 22:47:41 +0100

    create source handler functions, from dict based on source option

--- cache_albums.py
+++ cache_albums.py
@@ -8,22 +8,29 @@ import utils
 import collectors
 import auth
 
+def spotify_handler(opts):
+    spotify_auth_args = {'username': opts.spotify_username,
+                         'client_id': opts.spotify_client_id,
+                         'client_secret': opts.spotify_client_secret}
+    sp = auth.get_spotify_client(**spotify_auth_args)
+    sc = collectors.spotify.SpotifyCollector(sp)
+    spotify_albums = sc.collect()
+
+    return spotify_albums
+
+def files_handler(opts):
+    files_dir = os.path.expanduser(opts.files_dir)
+    file_albums = collectors.files.collect(files_dir)
+
+    return file_albums
+
 
 if __name__ == '__main__':
     opts = options.get_options()
 
-    if 'spotify' in opts.source:
-        spotify_auth_args = {'username': opts.spotify_username,
-                             'client_id': opts.spotify_client_id,
-                             'client_secret': opts.spotify_client_secret}
-        sp = auth.get_spotify_client(**spotify_auth_args)
-        sc = collectors.spotify.SpotifyCollector(sp)
-        spotify_albums = sc.collect()
+    handlers = {'spotify': spotify_handler, 'files': files_handler}
 
-        utils.write_albums(opts.cache_dir, 'spotify', spotify_albums)
+    albums_dict = {k: handlers[k](opts) for k in opts.source}
 
-    if 'files' in opts.source:
-        files_dir = os.path.expanduser(opts.files_dir)
-        file_albums = collectors.files.collect(files_dir)
-
-        utils.write_albums(opts.cache_dir, 'files', file_albums)
+    for k, v in albums_dict.items():
+        utils.write_albums(opts.cache_dir, k, v)
